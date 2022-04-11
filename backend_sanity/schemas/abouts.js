eecd3bd4ff11e81050c259b81ba2401599c634cd
AmrHE <amr.hassan.emam@gmail.com>
2022-04-11 08:57:25 +0200

    update backend

--- backend_sanity/schemas/abouts.js
+++ backend_sanity/schemas/abouts.js
@@ -1,26 +1,31 @@
-export default{
-    name:'abouts',
-    title:'Abouts',
-    type: 'document',
-    fields:[
-        {
-            name:'title',
-            title:'Title',
-            type:'string'
-        },
-        {
-            name:'description',
-            title:'Description',
-            type:'string'
-        },
-        {
-            name:'imgUrl',
-            title:'ImgUrl',
-            type: 'image',
-            options: {
-              hotspot: true,
-            },
-        },
-        
-    ]
-}
+export default {
+	name: 'abouts',
+	title: 'Abouts',
+	type: 'document',
+	fields: [
+		{
+			name: 'title',
+			title: 'Title',
+			type: 'string',
+		},
+		{
+			name: 'description',
+			title: 'Description',
+			type: 'string',
+		},
+		{
+			name: 'imgUrl',
+			title: 'ImgUrl',
+			type: 'image',
+			options: {
+				hotspot: true,
+			},
+		},
+		{
+			name: 'order',
+			title: 'Order',
+			type: 'number',
+			hidden: true,
+		},
+	],
+};
--- backend_sanity/schemas/skills.js
+++ backend_sanity/schemas/skills.js
@@ -1,26 +1,31 @@
-export default{
-    name:'skills',
-    title:'Skills',
-    type: 'document',
-    fields:[
-        {
-            name:'name',
-            title:'Name',
-            type:'string'
-        },
-        {
-            name:'bgColor',
-            title:'BgColor',
-            type:'string'
-        },
-        {
-            name:'icon',
-            title:'Icon',
-            type: 'image',
-            options: {
-              hotspot: true,
-            },
-        },
-        
-    ]
-}
+export default {
+	name: 'skills',
+	title: 'Skills',
+	type: 'document',
+	fields: [
+		{
+			name: 'name',
+			title: 'Name',
+			type: 'string',
+		},
+		{
+			name: 'bgColor',
+			title: 'BgColor',
+			type: 'string',
+		},
+		{
+			name: 'icon',
+			title: 'Icon',
+			type: 'image',
+			options: {
+				hotspot: true,
+			},
+		},
+		{
+			name: 'order',
+			title: 'Order',
+			type: 'number',
+			hidden: true,
+		},
+	],
+};
